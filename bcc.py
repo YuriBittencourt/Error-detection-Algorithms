d71985bbc31358b5915a6d5799e01ee4d29283b8
yuriBittencourt <yuri.bittencourt@acad.pucrs.br>
2019-04-24 11:32:49 -0300

    Avanço, existe um erro com números

--- bcc.py
+++ bcc.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import sys
 import utils
 
@@ -8,22 +9,36 @@ def get_n(n, str_lst):
         new_str += item[n]
     return new_str
 
-text = "redes"
-lst = []
-for letter in text:
-    binario = utils.to_bin(letter)
-    lst.append(binario + utils.paridade(binario))
+#Recebe String de texto e retorna a codificacao BCC correspondente
+def encode(text):
+    #transforma texto em binario
+    bin_list = list(map(utils.to_bin, text))
 
-parity = ""
-for i in range(0,8):
-    parity += utils.paridade(get_n(i, lst))
+    #adiciona o bit de paridade em cada letra que são 7 bits
+    for i in range(0, len(bin_list)):
+        bin_list[i] += utils.parity(bin_list[i])
 
-lst.append(parity)
-print(lst)
+    #calcula o bit de paridade da coluna 0 à 7
+    #ex         11011100
+    #           10111010
+    #paridade   01100110
+    parity=""
+    print(bin_list)
+    for i in range(0, 8):
+        parity += utils.parity(get_n(i, bin_list))
+        print(parity)
 
 
-#if(sys.argv[1] == '-e'):
-#    '''ENCODE'''
+    return 0
 
-#if(sys.argv[1] == '-d'):
-#    '''DECODE'''
+
+def decode(text):
+    return 0
+
+
+if sys.argv[1] == '-e':
+    print(encode(sys.argv[2]))
+
+if sys.argv[1] == '-d':
+    print(decode(sys.argv[2]))
+
