d2ab06b2d472a5b80e958dc7dcf9bf095545eefb
yuriBittencourt <yuri.bittencourt@acad.pucrs.br>
2019-04-24 18:02:19 -0300

    decoder feito
    Tem erro na verificação de coluna

--- bcc.py
+++ bcc.py
@@ -3,7 +3,7 @@ import sys
 import utils
 
 
-#Pega a coluna n de str_lst.
+# Pega a coluna n de str_lst.
 def get_col(n, str_lst):
     new_str = ""
     for item in str_lst:
@@ -11,33 +11,54 @@ def get_col(n, str_lst):
     return new_str
 
 
-#Recebe String de texto e retorna a codificacao BCC correspondente
+# Recebe String de texto e retorna a codificacao BCC correspondente
 def encode(text):
-    #transforma texto em binario
+    # transforma texto em binario
     bin_list = [utils.char_to_bin(c, 7) for c in text]
 
-    #adiciona o bit de paridade em cada letra que são 7 bits
+    # adiciona o bit de paridade em cada letra que são 7 bits
     for i in range(0, len(bin_list)):
         bin_list[i] += utils.parity(bin_list[i])
 
-    #calcula o bit de paridade da coluna 0 à 7
-    #ex         11011100
-    #           10111010
-    #paridade   01100110
+    # calcula o bit de paridade da coluna 0 à 7
+    # ex         11011100
+    #            10111010
+    # paridade   01100110
     parity=""
     for i in range(0, 8):
         parity += utils.parity(get_col(i, bin_list))
 
-    #Ler cada String de 8bits da lista e converter em hexadecimal e guardar na resultString.
-    resultString = ""
+    # Ler cada String de 8bits da lista e converter em hexadecimal e guardar na resultString.
+    result_string = ""
     for b in bin_list:
-        resultString += utils.bin_to_hex(b)
+        result_string += utils.bin_to_hex(b)
 
-    return resultString
+    return result_string
 
 
 def decode(text):
-    return 0
+    # Quebrar o text em strings binárias de 8bits
+    block_size = 2
+    bin_list = [utils.hex_to_bin(text[i:i + block_size], 8) for i in range(0, len(text), block_size)]
+
+    # Checar se as paridades batem
+    for b in bin_list:
+        if utils.parity(b[:-1]) != b[-1:]:
+            return "ERRO"
+
+    # Checar paridade das colunas
+    for i in range(0, 8):
+        binary_col = get_col(i, bin_list)
+        if utils.parity(binary_col[:-1]) != binary_col[-1:]:
+            print(binary_col,)
+            return "ERRO"
+
+    # Não há erros, retornar a mensagem correta.
+    result_string = ""
+    for b in bin_list:
+        result_string += utils.bin_to_ascii(b)
+
+    return result_string
 
 
 if sys.argv[1] == '-e':
