d2d60152d66b563d2be6eb689556eae8c5a86544
viniCerutti <vinicius.cerutti@edu.pucrs.br>
2019-04-26 21:09:08 -0300

    Adicionado argumentos

--- hamming.py
+++ hamming.py
@@ -1,4 +1,5 @@
 import utils
+import sys
 
 # n = limite superior
 # -1 pois como no hamming se trabalha com indices de 1...n
@@ -21,7 +22,7 @@ def __get_index_elemt_equals_one__(binary):
 def encode_calc_hamming(binary):
     # inverte o binario
     bin_inv = list(binary[::-1])
-
+    #print(len(bin_inv))
     # para cada elemento 2^1 colocar 0
     for i in range(len(bin_inv)):
         bin_inv.insert((2**i)-1,"0")
@@ -44,6 +45,7 @@ def encode_calc_hamming(binary):
 def decode_calc_hamming(binary):
     # inverte o binario
     bin_inv = list(binary[::-1])
+    #print(len(bin_inv))
     # pegar os indices que possuem 1
     index_list = __get_index_elemt_equals_one__(bin_inv)
     # pega as colunas de bits
@@ -70,7 +72,7 @@ def encode(string):
         # de cada binario realizar o hamming
         message_encode += encode_calc_hamming(binary)
 
-    print(message_encode.upper())
+    return message_encode.upper()
 
 def decode(string):
     # converter cada letra para binario
@@ -83,12 +85,18 @@ def decode(string):
         if error != 0:
             erros.append((letter,error))
 
-    print(message)
-    for letter, error in erros:
-        print("ERRO no caractere {} -> Correção: {}".format(message.index(letter),letter))
-
+    return {'message':message, 'erros': erros}
+"""
+encode("maxprint celular telefone 1928312 ,. ! # awidd")
+print(utils.hex_to_bin("6676067CB78079964D6797AA28261F62C6607AD6606067992827AA62C66062C63267E67962C28230434F31A34831D30431A2822E32FA28228528229C2826067B464D62B62B",0))
+decode("6676067CB78079964D6797AA28261F62C6607AD6606067992827AA62C66062C63267E67962C28230434F31A34831D30431A2822E32FA28228528229C2826067B464D62B62B")
+for i in range (0,len(b)):
+    aux = b[:]
+    aux[i] = '1' if aux[i] == '0' else '0'
+    string = "".join(aux)
+    decode(utils.bin_to_hex(string))
+"""
 # codificação com problemas
-
 #853513047F8
 #E229B356
 #FC5366354FC7D530462C
@@ -96,3 +104,12 @@ def decode(string):
 #9D2CE618
 #CE4B732E
 #857CB7B45312D063264A
+
+if sys.argv[1] == '-e':
+    print(encode(sys.argv[2]))
+
+if sys.argv[1] == '-d':
+    dec = decode(sys.argv[2])
+    print(dec['message'])
+    for letter, error in dec['erros']:
+        print("ERRO no caractere {} -> Correção: {}".format(dec['message'].index(letter)+1,letter))
