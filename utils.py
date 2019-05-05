1fe4ef62378ca6d3a66c204db441a639d8cbe87e
viniCerutti <vinicius.cerutti@edu.pucrs.br>
2019-05-05 17:16:51 -0300

    :rocket: :dizzy: estruturado comentarios

--- utils.py
+++ utils.py
@@ -1,5 +1,11 @@
 """
-Método que calcula a paridade de n, retorna "1" se ímpar, caso contrário retorna "0".
+Autores: Vinicius Cerutti e Yuri Bittencourt
+Trabalho de Introdução a Redes de Computadores 2019/1
+"""
+
+"""
+Metodo que calcula a paridade de n, retorna "1" se impar,
+caso contrário retorna "0".
 """
 def parity(n):
     if n.count("1") % 2 == 0:
@@ -7,37 +13,42 @@ def parity(n):
     return "1"
 
 """
-Método que converte uma string de caracteres para string de binários com padding definido.
+Metodo que converte uma string de caracteres para string de 
+binários com padding definido.
 """
 def char_to_bin(letter, padding):
     return (bin(ord(letter))[2:]).zfill(padding)
 
 """
-Método que converte um inteiro para string de binários com padding definido.
+Metodo que converte um inteiro para string de binarios com padding 
+definido.
 """
 def int_to_bin(number, padding):
     return (bin(number)[2:]).zfill(padding)
 
 """
-Método que converte uma string hexadecimal para string de binários com padding definido.
+Metodo que converte uma string hexadecimal para string de binarios 
+com padding definido.
 """
 def hex_to_bin(number, padding):
     return bin(int(number, 16))[2:].zfill(padding)
 
 """
-Método que converte uma string de binários para string de caracteres ASCII.
+Metodo que converte uma string de binarios para string 
+de caracteres ASCII.
 """
 def bin_to_ascii(binary):
     return chr(int(binary, 2))
 
 """
-Método que converte uma string de binários para string de hexadecimais.
+Metodo que converte uma string de binarios para 
+string de hexadecimais.
 """
 def bin_to_hex(binary):
     return hex(int(binary, 2))[2:]
 
 """
-Método que converte uma string de binários para um inteiro.
+Metodo que converte uma string de binarios para um inteiro.
 """
 def bin_to_int(binary):
 	return int(binary,2)
