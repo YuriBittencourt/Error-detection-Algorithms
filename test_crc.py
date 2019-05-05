35e0b9aa5ad2b026ed6fcd48d5c3946e1c357626
YuriBittencourt <yuri.bittencourt@acad.pucrs.br>
2019-05-05 19:04:34 -0300

    refinamento

--- test_crc.py
+++ test_crc.py
@@ -3,17 +3,27 @@ import crc
 import utils
 import sys
 
-#teste sem erros
+# testa encode-decode sem erros
 if sys.argv[1] == '-n':
     with open(sys.argv[2], "r") as f:
+        failures = []
+        count = 0
         for line in f:
+            count += 1
             try:
                 line = line.replace("\n","")
                 test = crc.encode(line, sys.argv[3])
                 test = crc.decode(test, sys.argv[3])
                 assert test['message'] == line
-                print(line, 'success')
 
             except AssertionError:
-                print(line, 'failure')
+                failures.append(line)
 
+        print("Finished with {}/{} successes".format(count-len(failures),count))
+        if len(failures):
+            print(','.join(failures))
+
+
+# testa decode com erros
+if sys.argv[1] == '-w':
+    pass
